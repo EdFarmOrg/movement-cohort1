93d32d314ed77830d482d5740b5b83c559116f8e
Santita Humphries <santitahumphries@gmail.com>
2023-02-27 11:11:54 -0600

    Added Map and locations

--- MovementCohort1/MovementCohort1/Views/Magic Walks/WalksViewController.swift
+++ MovementCohort1/MovementCohort1/Views/Magic Walks/WalksViewController.swift
@@ -1,30 +1,67 @@
-//
-//  WalksViewController.swift
-//  MovementCohort1
-//
-//  Created by Craig Clayton on 1/30/23.
-//
-
 import UIKit
+import MapKit
 
-class WalksViewController: UIViewController {
-
+class MagicWalksViewController: UIViewController {
+    let birmingham = CLLocationCoordinate2D(latitude: 33.543682, longitude: -86.779633)
+    let locations = [["title": "EdFarm", "latitude": 33.517070, "longitude": -86.808441],
+        ["title":"Fred Shuttlesworth Statue", "latitude": 33.530050, "longitude": -86.826680],
+        ["title":"St. Paul’s Cathedral", "latitude": 33.517860, "longitude": -86.804820]
+    ]
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initialize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initialize() {
+        setupMapView()
+        addCustomPin()
     }
-    */
+    
+    private func setupMapView() {
+        mapView.overrideUserInterfaceStyle =
+            .dark
+        mapView.setRegion(MKCoordinateRegion(center: birmingham, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)), animated: false)
+//        mapView.delegate = self
+    }
+    
+    private func addCustomPin() {
+        for location in locations {
+            let annotations = MKPointAnnotation()
+            annotations.title = location["title"] as? String
+            let pins = CLLocationCoordinate2D(latitude: location["latitude"] as! Double, longitude: location["longitude"] as! Double)
+            
+            annotations.coordinate = pins
+            mapView.addAnnotation(annotations)
+            
+        }
+       
+    }
+    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        guard !(annotation is MKUserLocation) else {
+//            return nil
+//        }
+//
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
+//
+//        if annotationView == nil {
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+//            annotationView?.canShowCallout = true
+//        } else {
+//            annotationView?.annotation = annotation
+//        }
+//
+////        annotationView?.image = UIImage(named: "edFarm2")
+//
+//        return annotationView
+//    }
+
+
+
+    
 
 }
 
@@ -38,7 +75,6 @@ struct WalksViewControllerRepresentable: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        
     }
 }
 
